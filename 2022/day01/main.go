867350445c4c2360a165062f882901f0b1482e6d
Jonas <jonas.fo@live.se>
2022-12-02 19:51:44 +0100

    Second draft

--- 2022/day01/main.go
+++ 2022/day01/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "strconv"
+import "fmt"
+
+// import "strconv"
+
+var elf_nr int = 0
 
 func findCalories(args ...[]string) (int, int) {
 	max := 0
@@ -18,7 +22,9 @@ func findCalories(args ...[]string) (int, int) {
 func sumArrayValues(array []string) int {
 	sum := 0
 	for i := 0; i < len(array); i++ {
-		sum += strconv.ParseInt(array[i])
+		temp := array[i]
+		fmt.Println(temp)
+		// sum += strconv.ParseInt(temp)
 	}
 	return sum
 }
@@ -26,24 +32,32 @@ func sumArrayValues(array []string) int {
 func splitInventory(inventory []string) {
 	min_index := 0
 	max_index := 0
-	return_array := []string{}
+	// return_array := []string{}
+
+	largest_cal := 0
+	summed_cal := 0
 	for i := 0; i < len(inventory); i++ {
 		if inventory[i] == "" {
 			max_index = i
 			if max_index != 0 {
-				for j := min_index; j < max_index; j++ {
-					append(return_array, inventory[min_index:max_index])
-				}
+				slice_arr := inventory[min_index:max_index]
+				summed_cal = sumArrayValues(slice_arr)
 			} else {
-				for j := 0; j < max_index; j++ {
-					append(return_array, inventory[0:max_index])
-				}
+				slice_arr := inventory[0:max_index]
+				summed_cal = sumArrayValues(slice_arr)
+				// temp_arr := make([]string, max_index)
+				// copy(temp_arr, slice_arr)
+				// return_array = append(return_array, temp_arr)
 			}
-
+			min_index = i
+		}
+		if summed_cal > largest_cal {
+			elf_nr = i
 		}
 	}
 }
 
 func main() {
 	inventory := []string{"1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
+	splitInventory(inventory)
 }
