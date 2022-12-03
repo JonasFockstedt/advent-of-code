0274956f7ff51463ec443f54d7447dece6cf4de7
Jonas <jonas.fo@live.se>
2022-12-03 13:30:34 +0100

    Day 2 complete

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -12,9 +12,9 @@ import (
 A - Opponent Rock
 B - Opponent Paper
 C - Opponent Scissor
-X - Me Rock
-Y - Me Paper
-Z - Me Scissor
+X - I need to lose
+Y - Needs to be a draw
+Z - I need to win
 
 1 point for Rock, 2 points for Paper, 3 points for Scissors
 0 points for loss, 3 points for draw, 6 points for win
@@ -28,24 +28,24 @@ Input of game:
 func decideWinner(round [][]string) {
 	points := 0
 	for i := 0; i < len(round); i++ {
-		if round[i][0] == "A" && round[i][1] == "X" { // Opponent rock, me rock
-			points += 4
-		} else if round[i][0] == "A" && round[i][1] == "Y" { // Opponent rock, me paper
-			points += 8
-		} else if round[i][0] == "A" && round[i][1] == "Z" { // Opponent rock, me scissor
+		if round[i][0] == "A" && round[i][1] == "X" { // Opponent rock, I need to lose, so I pick scissors
 			points += 3
-		} else if round[i][0] == "B" && round[i][1] == "X" { // Opponent paper, me rock
+		} else if round[i][0] == "A" && round[i][1] == "Y" { // Opponent rock, needs to be a draw, I pick rock
+			points += 4
+		} else if round[i][0] == "A" && round[i][1] == "Z" { // Opponent rock, I need to win, so I pick papers
+			points += 8
+		} else if round[i][0] == "B" && round[i][1] == "X" { // Opponent paper, I need to lose, so I pick rock
 			points += 1
-		} else if round[i][0] == "B" && round[i][1] == "Y" { // Opponent paper, me paper
+		} else if round[i][0] == "B" && round[i][1] == "Y" { // Opponent paper, needs to be a draw, so I pick papers
 			points += 5
-		} else if round[i][0] == "B" && round[i][1] == "Z" { // Opponent paper, me scissor
+		} else if round[i][0] == "B" && round[i][1] == "Z" { // Opponent paper, I need to win, so I pick scissors
 			points += 9
-		} else if round[i][0] == "C" && round[i][1] == "X" { // Opponent scissor, me rock
-			points += 7
-		} else if round[i][0] == "C" && round[i][1] == "Y" { // Opponent scissor, me paper
+		} else if round[i][0] == "C" && round[i][1] == "X" { // Opponent scissor, I need to lose, so I pick papers
 			points += 2
-		} else if round[i][0] == "C" && round[i][1] == "Z" { // Opponent scissor, me scissor
+		} else if round[i][0] == "C" && round[i][1] == "Y" { // Opponent scissor, needs to be a draw, so I pick scissors
 			points += 6
+		} else if round[i][0] == "C" && round[i][1] == "Z" { // Opponent scissor, I need to win, so I pick rock
+			points += 7
 		}
 	}
 	fmt.Printf("Estimated points from this match are %d", points)
