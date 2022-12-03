d26a8a977ef01f4d1f8fe13914afbff2db49988f
Jonas <jonas.fo@live.se>
2022-12-03 07:10:30 +0100

    Completed first half of day 1

--- 2022/day01/main.go
+++ 2022/day01/main.go
@@ -7,19 +7,6 @@ import (
 
 var elf_most_cal int = 0
 
-func findCalories(args ...[]string) (int, int) {
-	max := 0
-	index := 0
-	for i := 0; i < len(args); i++ {
-		temp_array := sumArrayValues(args[i])
-		if temp_array > max {
-			max = temp_array
-			index = i
-		}
-	}
-	return max, index
-}
-
 func sumArrayValues(array []string) int {
 	sum := 0
 	integer := 0
@@ -31,7 +18,7 @@ func sumArrayValues(array []string) int {
 	return sum
 }
 
-func splitInventory(inventory []string) {
+func findElf(inventory []string) int {
 	min_index := 0
 	max_index := 0
 	largest_cal := 0
@@ -43,26 +30,29 @@ func splitInventory(inventory []string) {
 			max_index = i
 			if max_index != 0 {
 				slice_arr := inventory[min_index:max_index]
-				// fmt.Println(slice_arr)
 				summed_cal = sumArrayValues(slice_arr)
-				fmt.Println(summed_cal)
 			} else {
 				slice_arr := inventory[0:max_index]
-				// fmt.Println(slice_arr)
 				summed_cal = sumArrayValues(slice_arr)
-				fmt.Println(summed_cal)
 
 			}
-			min_index = i
+			min_index = i + 1
+		}
+		if i == len(inventory)-1 { // If the second last element of the array is an empty string, and the last element is a value
+			elf_nr += 1
+			last_element := inventory[len(inventory)-1]
+			summed_cal, _ = strconv.Atoi(last_element)
 		}
 		if summed_cal > largest_cal {
+			largest_cal = summed_cal
 			elf_most_cal = elf_nr
 		}
 	}
+	return largest_cal
 }
 
 func main() {
 	inventory := []string{"1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
-	splitInventory(inventory)
-	fmt.Printf("Elf number %d is carrying the most calories", elf_most_cal)
+	most_cal := findElf(inventory)
+	fmt.Printf("Elf number %d is carrying the most calories (%d)", elf_most_cal, most_cal)
 }
